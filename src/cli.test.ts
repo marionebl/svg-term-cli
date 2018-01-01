1225f43764a83d154d34c0d75952ba4d909e2a86
Mario Nebl <hello@mario-nebl.de>
2018-01-01 14:49:00 +0100

    chore: add tslint/prettier setup

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,35 +1,43 @@
-import * as path from 'path';
-import * as execa from 'execa';
+import * as execa from "execa";
+import * as path from "path";
 
-const pkg = require('../package');
+const pkg = require("../package");
 
 const bin = async (args: string[] = [], options = {}) => {
   try {
-    return await execa('ts-node', [path.join(__dirname, './cli.ts'), ...args], options);
+    return await execa(
+      "ts-node",
+      [path.join(__dirname, "./cli.ts"), ...args],
+      options
+    );
   } catch (err) {
     return err;
   }
 };
 
-test('prints help with non-zero exit code', async () => {
-  const result = await bin([], {input: ''});
+test("prints help with non-zero exit code", async () => {
+  const result = await bin([], { input: "" });
   expect(result.code).not.toBe(0);
-  expect(result.stdout).toContain('svg-term: either stdin or --cast are required');
+  expect(result.stdout).toContain(
+    "svg-term: either stdin or --cast are required"
+  );
 });
 
-test('prints help with zero exit code for --help', async () => {
-  const result = await bin(['--help'], {input: ''});
+test("prints help with zero exit code for --help", async () => {
+  const result = await bin(["--help"], { input: "" });
   expect(result.code).toBe(0);
-  expect(result.stdout).toContain('print this help');
+  expect(result.stdout).toContain("print this help");
 });
 
-test('prints version with zero exit code for --version', async () => {
-  const result = await bin(['--version'], {input: ''});
+test("prints version with zero exit code for --version", async () => {
+  const result = await bin(["--version"], { input: "" });
   expect(result.code).toBe(0);
   expect(result.stdout).toBe(pkg.version);
 });
 
-test('works for minimal stdin input', async () => {
-  const result = await bin([], {input: '[{"version": 2, "width": 1, "height": 1}, [1, "o", "foo"]]'});
+test("works for minimal stdin input", async () => {
+  const result = await bin([], {
+    input: '[{"version": 2, "width": 1, "height": 1}, [1, "o", "foo"]]'
+  });
   expect(result.code).toBe(0);
 });
--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
-import {guessTerminal, GuessedTerminal} from 'guess-terminal';
-import * as macosAppConfig from 'macos-app-config';
-import * as meow from 'meow';
-import * as parsers from 'term-schemes';
-import {TermSchemes, TermScheme} from 'term-schemes';
+import * as fs from "fs";
+import { GuessedTerminal, guessTerminal } from "guess-terminal";
+import * as macosAppConfig from "macos-app-config";
+import * as meow from "meow";
+import * as os from "os";
+import * as path from "path";
+import * as parsers from "term-schemes";
 
-const plist = require('plist');
-const fetch = require('node-fetch');
-const getStdin = require('get-stdin');
-const {render} = require('svg-term');
-const sander = require('@marionebl/sander');
-const SVGO = require('svgo');
+const plist = require("plist");
+const fetch = require("node-fetch");
+const getStdin = require("get-stdin");
+const { render } = require("svg-term");
+const sander = require("@marionebl/sander");
+const SVGO = require("svgo");
 
 interface Guesses {
   [key: string]: string | null;
@@ -26,10 +25,12 @@ interface SvgTermCli {
 }
 
 interface SvgTermError extends Error {
-  help: () => string;
+  help(): string;
 }
 
-withCli(main, `
+withCli(
+  main,
+  `
   Usage
     $ svg-term [options]
 
@@ -55,98 +56,125 @@ withCli(main, `
     $ echo rec.json | svg-term 
     $ svg-term --cast 113643 
     $ svg-term --cast 113643 --out examples/parrot.svg
-`);
+`
+);
 
 async function main(cli: SvgTermCli) {
   const input = await getInput(cli);
   const error = cliError(cli);
-  
+
   if (!input) {
     throw error(`svg-term: either stdin or --cast are required`);
   }
 
-  const malformed = ensure(['height', 'width'], cli.flags, (name, val) => {
+  const malformed = ensure(["height", "width"], cli.flags, (name, val) => {
     if (!(name in cli.flags)) {
-      return;
+      return null;
     }
+
     const candidate = parseInt(val, 10);
     if (isNaN(candidate)) {
       return new TypeError(`${name} expected to be number, received "${val}"`);
     }
+
+    return null;
   });
 
   if (malformed.length > 0) {
-    throw error(`svg-term: ${malformed.map(m => m.message).join('\n')}`);
+    throw error(`svg-term: ${malformed.map(m => m.message).join("\n")}`);
   }
 
-  const missingValue = ensure(['cast', 'out', 'profile'], cli.flags, (name, val) => {
-    if (!(name in cli.flags)) {
-      return;
-    }
-    if (name === 'cast' && typeof val === 'number') {
-      return;
-    }
-    if (typeof val !== 'string') {
+  const missingValue = ensure(
+    ["cast", "out", "profile"],
+    cli.flags,
+    (name, val) => {
+      if (!(name in cli.flags)) {
+        return null;
+      }
+      if (name === "cast" && typeof val === "number") {
+        return null;
+      }
+      if (typeof val === "string") {
+        return null;
+      }
+
       return new TypeError(`${name} expected to be string, received "${val}"`);
     }
-  });
+  );
 
   if (missingValue.length > 0) {
-    throw error(`svg-term: ${missingValue.map(m => m.message).join('\n')}`);
+    throw error(`svg-term: ${missingValue.map(m => m.message).join("\n")}`);
   }
 
-  const shadowed = ensure(['at', 'from', 'to'], cli.flags, (name, val) => {
+  const shadowed = ensure(["at", "from", "to"], cli.flags, (name, val) => {
     if (!(name in cli.flags)) {
-      return;
+      return null;
     }
-    if (typeof val !== 'number' || isNaN(val)) {
+    if (typeof val !== "number" || isNaN(val)) {
       return new TypeError(`${name} expected to be number, received "${val}"`);
     }
-    if (name !== 'at' && typeof cli.flags.at === 'number') {
+    if (name !== "at" && typeof cli.flags.at === "number") {
       return new TypeError(`--at flag disallows --${name}`);
     }
+
+    return null;
   });
 
   if (shadowed.length > 0) {
-    throw error(`svg-term: ${shadowed.map(m => m.message).join('\n')}`);
+    throw error(`svg-term: ${shadowed.map(m => m.message).join("\n")}`);
   }
 
   const term = guessTerminal() || cli.flags.term;
-  const profile = term ? guessProfile(term) || cli.flags.profile : cli.flags.profile;
+  const profile = term
+    ? guessProfile(term) || cli.flags.profile
+    : cli.flags.profile;
 
   const guess: Guesses = {
     term,
     profile
   };
 
-  if (('term' in cli.flags) || ('profile' in cli.flags)) {
-    const unsatisfied = ['term', 'profile'].filter(n => !Boolean(guess[n]));
+  if ("term" in cli.flags || "profile" in cli.flags) {
+    const unsatisfied = ["term", "profile"].filter(n => !Boolean(guess[n]));
 
     if (unsatisfied.length > 0) {
-      throw error(`svg-term: --term and --profile must be used together, ${unsatisfied.join(', ')} missing`);
+      throw error(
+        `svg-term: --term and --profile must be used together, ${unsatisfied.join(
+          ", "
+        )} missing`
+      );
     }
   }
 
-  const unknown = ensure(['term'], cli.flags, (name, val) => {
+  const unknown = ensure(["term"], cli.flags, (name, val) => {
     if (!(name in cli.flags)) {
-      return;
+      return null;
     }
-    if (!(cli.flags.term in TermSchemes)) {
-      return new TypeError(`${name} expected to be one of ${Object.keys(TermSchemes).join(', ')}, received "${val}"`);
+
+    if (!(cli.flags.term in parsers.TermSchemes)) {
+      return null;
     }
+
+    return new TypeError(
+      `${name} expected to be one of ${Object.keys(parsers.TermSchemes).join(
+        ", "
+      )}, received "${val}"`
+    );
   });
 
   if (unknown.length > 0) {
-    throw error(`svg-term: ${unknown.map(m => m.message).join('\n')}`);
+    throw error(`svg-term: ${unknown.map(m => m.message).join("\n")}`);
   }
 
-  const p = guess.profile || '';
-  const isFileProfile = ['~', '/', '.'].indexOf(p.charAt(0)) > -1;
+  const p = guess.profile || "";
+  const isFileProfile = ["~", "/", "."].indexOf(p.charAt(0)) > -1;
 
-  if (isFileProfile && 'profile' in cli.flags) {
+  if (isFileProfile && "profile" in cli.flags) {
     const missing = !fs.existsSync(path.join(process.cwd(), cli.flags.profile));
     if (missing) {
-      throw error(`svg-term: ${cli.flags.profile} must be readable file but was not found`);
+      throw error(
+        `svg-term: ${cli.flags.profile} must be readable file but was not found`
+      );
     }
   }
 
@@ -154,7 +182,7 @@ async function main(cli: SvgTermCli) {
 
   const svg = render(input, {
     at: toNumber(cli.flags.at),
-    cursor: toBoolean(cli.flags.cursor, true), 
+    cursor: toBoolean(cli.flags.cursor, true),
     from: toNumber(cli.flags.from),
     paddingX: toNumber(cli.flags.paddingX || cli.flags.padding),
     paddingY: toNumber(cli.flags.paddingY || cli.flags.padding),
@@ -166,34 +194,42 @@ async function main(cli: SvgTermCli) {
   });
 
   const svgo = new SVGO({
-    plugins: [
-      {collapseGroups: false}
-    ]
+    plugins: [{ collapseGroups: false }]
   });
 
-  const optimized = toBoolean(cli.flags.optimize, true) ? await svgo.optimize(svg) : svg;
+  const optimized = toBoolean(cli.flags.optimize, true)
+    ? await svgo.optimize(svg)
+    : svg;
 
-  if (typeof cli.flags.out === 'string') {
+  if (typeof cli.flags.out === "string") {
     sander.writeFile(cli.flags.out, Buffer.from(optimized.data));
   } else {
     process.stdout.write(optimized);
   }
 }
 
-function ensure(names: string[], flags: SvgTermCli['flags'], predicate: (name: string, val: any) => Error | null): Error[] {
-  return names.map(name => predicate(name, flags[name])).filter(e => e instanceof Error);
+function ensure(
+  names: string[],
+  flags: SvgTermCli["flags"],
+  predicate: (name: string, val: any) => Error | null
+): Error[] {
+  return names
+    .map(name => predicate(name, flags[name]))
+    .filter(e => e instanceof Error)
+    .map(e => e as Error);
 }
 
 function cliError(cli: SvgTermCli): (message: string) => SvgTermError {
-  return (message: string): SvgTermError => {
+  return message => {
     const err: any = new Error(message);
-    err.help = () => cli.help; 
+    err.help = () => cli.help;
+
     return err;
   };
 }
 
 function getConfig(term: GuessedTerminal): any {
-  switch(term) {
+  switch (term) {
     case GuessedTerminal.terminal: {
       return macosAppConfig.sync(term)[0];
     }
@@ -208,12 +244,12 @@ function getConfig(term: GuessedTerminal): any {
 function getPresets(term: GuessedTerminal): any {
   const config = getConfig(term);
 
-  switch(term) {
+  switch (term) {
     case GuessedTerminal.terminal: {
-      return config['Window Settings'];
+      return config["Window Settings"];
     }
     case GuessedTerminal.iterm2: {
-      return config['Custom Color Presets'];
+      return config["Custom Color Presets"];
     }
     default:
       return null;
@@ -221,7 +257,7 @@ function getPresets(term: GuessedTerminal): any {
 }
 
 function guessProfile(term: GuessedTerminal): string | null {
-  if (os.platform() !== 'darwin') {
+  if (os.platform() !== "darwin") {
     return null;
   }
 
@@ -231,12 +267,12 @@ function guessProfile(term: GuessedTerminal): string | null {
     return null;
   }
 
-  switch(term) {
+  switch (term) {
     case GuessedTerminal.terminal: {
-      return config['Default Window Settings'];
+      return config["Default Window Settings"];
     }
     case GuessedTerminal.iterm2: {
-      const presets = config['Custom Color Presets'];
+      return null;
     }
     default:
       return null;
@@ -245,78 +281,84 @@ function guessProfile(term: GuessedTerminal): string | null {
 
 async function getInput(cli: SvgTermCli) {
   if (cli.flags.cast) {
-    const response = await fetch(`https://asciinema.org/a/${cli.flags.cast}.cast?dl=true`);
+    const response = await fetch(
+      `https://asciinema.org/a/${cli.flags.cast}.cast?dl=true`
+    );
+
     return response.text();
   }
 
-  return await getStdin();  
+  return getStdin();
 }
 
 function getParser(term: string) {
-  switch(term) {
-    case TermSchemes.iterm2:
+  switch (term) {
+    case parsers.TermSchemes.iterm2:
       return parsers.iterm2;
-    case TermSchemes.konsole:
+    case parsers.TermSchemes.konsole:
       return parsers.konsole;
-    case TermSchemes.remmina:
+    case parsers.TermSchemes.remmina:
       return parsers.remmina;
-    case TermSchemes.terminal:
+    case parsers.TermSchemes.terminal:
       return parsers.terminal;
-    case TermSchemes.terminator:
+    case parsers.TermSchemes.terminator:
       return parsers.terminator;
-    case TermSchemes.termite:
+    case parsers.TermSchemes.termite:
       return parsers.termite;
-    case TermSchemes.tilda:
+    case parsers.TermSchemes.tilda:
       return parsers.tilda;
-    case TermSchemes.xcfe:
+    case parsers.TermSchemes.xcfe:
       return parsers.xfce;
-    case TermSchemes.xresources:
+    case parsers.TermSchemes.xresources:
       return parsers.xresources;
-    case TermSchemes.xterm:
+    case parsers.TermSchemes.xterm:
       return parsers.xterm;
     default:
       throw new Error(`unknown term parser: ${term}`);
   }
 }
 
-function getTheme(guess: Guesses): TermScheme | null {
+function getTheme(guess: Guesses): parsers.TermScheme | null {
   if (guess.term === null || guess.profile === null) {
     return null;
   }
-  
-  const p = guess.profile || '';
-  const isFileProfile = ['~', '/', '.'].indexOf(p.charAt(0)) > -1;
 
-  return isFileProfile 
-    ? parseTheme(guess.term, guess.profile) 
-    : extractTheme(guess.term, guess.profile);
+  const p = guess.profile || "";
+  const isFileProfile = ["~", "/", "."].indexOf(p.charAt(0)) > -1;
+
+  return isFileProfile
+    ? parseTheme(guess.term as string, guess.profile as string)
+    : extractTheme(guess.term as string, guess.profile as string);
 }
 
-function parseTheme(term: string, input: string): TermScheme {
+function parseTheme(term: string, input: string): parsers.TermScheme {
   const parser = getParser(term);
+
   return parser(String(fs.readFileSync(input)));
 }
 
-function extractTheme(term: string, name: string): TermScheme | null {
+function extractTheme(term: string, name: string): parsers.TermScheme | null {
   if (!(term in GuessedTerminal)) {
     return null;
   }
 
-  if (os.platform() !== 'darwin') {
+  if (os.platform() !== "darwin") {
     return null;
   }
 
   if (term === GuessedTerminal.hyper) {
-    const filename = path.resolve(os.homedir(), '.hyper.js');
-    const theme = parsers.hyper(String(fs.readFileSync(filename)), {filename});
-    return theme;
+    const filename = path.resolve(os.homedir(), ".hyper.js");
+
+    return parsers.hyper(String(fs.readFileSync(filename)), {
+      filename
+    });
   }
 
   const presets = getPresets(term as GuessedTerminal);
 
   if (!presets) {
     return null;
-  } 
+  }
 
   const theme = presets[name];
   const parser = getParser(term);
@@ -344,32 +386,36 @@ function toNumber(input: string | null): number | null {
   if (isNaN(candidate)) {
     return null;
   }
+
   return candidate;
 }
 
 function toBoolean(input: any, fb: boolean): boolean {
-  if (typeof input === 'undefined') {
+  if (input === undefined) {
     return fb;
   }
-  if (input === 'false') {
+  if (input === "false") {
     return false;
   }
-  if (input === 'true') {
+  if (input === "true") {
     return true;
   }
+
   return input === true;
 }
 
-function withCli(fn: (cli: SvgTermCli) => Promise<void>, help: string = ''): Promise<void> {
-  return main(meow(help))
-    .catch(err => {
-      setTimeout(() => {
-        if (typeof err.help === 'function') {
-          console.log(err.help());
-          console.log('\n', err.message);
-          process.exit(1);
-        }
-        throw err;
-      }, 0);
-    });
+function withCli(
+  fn: (cli: SvgTermCli) => Promise<void>,
+  help: string = ""
+): void {
+  fn(meow(help)).catch(err => {
+    setTimeout(() => {
+      if (typeof err.help === "function") {
+        console.log(err.help());
+        console.log("\n", err.message);
+        process.exit(1);
+      }
+      throw err;
+    }, 0);
+  });
 }
