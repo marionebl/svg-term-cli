8cd31d3b88f8bb96399a1ea92a6644438131b69e
Mario Nebl <hello@mario-nebl.de>
2018-02-16 19:11:08 +0100

    test: resync and expand harness

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,6 +1,9 @@
 import * as execa from "execa";
 import * as path from "path";
 
+const {DOMParser} = require('xmldom');
+
+const parser = new DOMParser();
 const pkg = require("../package");
 
 const bin = async (args: string[] = [], options = {}) => {
@@ -18,7 +21,7 @@ const bin = async (args: string[] = [], options = {}) => {
 test("prints help with non-zero exit code", async () => {
   const result = await bin([], { input: "" });
   expect(result.code).not.toBe(0);
-  expect(result.stdout).toContain(
+  expect(result.stderr).toContain(
     "svg-term: either stdin, --cast, --command or --in are required"
   );
 });
@@ -40,4 +43,42 @@ test("works for minimal stdin input", async () => {
     input: '[{"version": 2, "width": 1, "height": 1}, [1, "o", "foo"]]'
   });
   expect(result.code).toBe(0);
+});
+
+test("is silent on stderr for minimal stdin input", async () => {
+  const result = await bin([], {
+    input: '[{"version": 2, "width": 1, "height": 1}, [1, "o", "foo"]]'
+  });
+  expect(result.stderr).toBe("");
+  expect(result.code).toBe(0);
+});
+
+test("emits svg for minimal stdin input", async () => {
+  const result = await bin([], {
+    input: '[{"version": 2, "width": 1, "height": 1}, [1, "o", "foo"]]'
+  });
+
+  const doc = parser.parseFromString(result.stdout, 'image/svg+xml');
+  expect(doc.documentElement.tagName).toBe('svg');
+});
+
+test("fails for faulty stdin input", async () => {
+  const result = await bin([], {
+    input: '{}'
+  });
+  expect(result.code).toBe(1);
+});
+
+test("emits error on stderr for faulty stdin input", async () => {
+  const result = await bin([], {
+    input: '{}'
+  });
+  expect(result.stderr).toContain("only asciicast v1 and v2 formats can be opened");
+});
+
+test("is silent on stdin for faulty stdin input", async () => {
+  const result = await bin([], {
+    input: '{}'
+  });
+  expect(result.stdout).toBe("");
 });
